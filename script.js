887af3dfda7987cd1a50701da978f84abbdcfb10
Rohail <125988921+Rohail-Suii@users.noreply.github.com>
2023-05-24 20:10:34 +0500

    script adding start

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const date = document.querySelector('.date');
+const temp = document.querySelector('.temp');
+const city = document.querySelector('.city');
+const country = document.querySelector('.country');
+const description = document.querySelector('.description');
+const submit = document.querySelector('.submit_btn');
+
