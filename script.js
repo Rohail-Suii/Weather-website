f85259336a91a5decbc56d7921425c2240128d78
Rohail <125988921+Rohail-Suii@users.noreply.github.com>
2023-05-27 16:02:09 +0500

    Weather API start returning object

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-
-const date = document.querySelector('.date');
-const temp = document.querySelector('.temp');
-const city = document.querySelector('.city');
-const country = document.querySelector('.country');
+const inputBox = document.querySelector('.input-box');
+const searchBtn = document.getElementById('searchBtn');
+const weather_img = document.querySelector('.weather-img');
+const temperature = document.querySelector('.temperature');
 const description = document.querySelector('.description');
-const submit = document.querySelector('.submit_btn');
+const humidity = document.getElementById('humidity');
+const wind_speed = document.getElementById('wind-speed');
 
+const location_not_found = document.querySelector('.location-not-found');
+
+const weather_body = document.querySelector('.weather-body');
+
+
+async function checkWeather(city){
+    const api_key = "f7c4cee675738525b8c8aeb0a521d9fb";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+
+    const weather_data = await fetch(`${url}`).then(response => response.json());
+    console.log(weather_data);
+}
+
+
+searchBtn.addEventListener('click', ()=>{
+    checkWeather(inputBox.value);
+});
